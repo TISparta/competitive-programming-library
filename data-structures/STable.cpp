be0907cfa0d97920616be36d9606e171518cd55f
TISparta <jose.garcia@utec.edu.pe>
2021-01-01 22:44:34 -0500

    Suffix array and fixing sparse table

--- data-structures/STable.cpp
+++ data-structures/STable.cpp
@@ -1,5 +1,5 @@
 struct STable {
-  const int LG = 18;
+  const int LG = 20;
   vector <vi> st;
   
   inline int op (int x, int y) {
@@ -12,7 +12,7 @@ struct STable {
       st[i][0] = arr[i];
     }
     for (int bit = 1; bit < LG; bit++) {
-      for (int i = l; i + (1 << bit) <= r; i++) {
+      for (int i = l; i + (1 << bit) - 1 <= r; i++) {
         int j = i + (1 << (bit - 1));
         st[i][bit] = op(st[i][bit - 1], st[j][bit - 1]);
       }
@@ -22,8 +22,8 @@ struct STable {
   int query (int l, int r) {
     if (l == r) return st[l][0];
     int x = 31 - __builtin_clz(r - l);
-    int ll = r - (1 << x) + 1;
-    return op(st[l][x], st[ll][x]);
+    int l2 = r - (1 << x) + 1;
+    return op(st[l][x], st[l2][x]);
   }
 
 };
